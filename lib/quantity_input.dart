c2812d6de744f06298bdebedcb16d983885f31de
afifwandala <wafriz01@gmail.com>
2022-12-23 04:36:20 +0700

    Update quantity_input.dart

--- lib/quantity_input.dart
+++ lib/quantity_input.dart
@@ -189,7 +189,7 @@ class _QuantityInputState extends State<QuantityInput> {
                 }
               ),
               Container(
-                height: 22,
+                height: 25,
                 width: widget.inputWidth,
                 child: Padding(
                   padding: EdgeInsets.symmetric(horizontal: 5),
